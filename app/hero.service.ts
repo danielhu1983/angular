77536f657b2ab9ea48dc795a3ebcf0832880d26d
Daniel Hu <danielhu83@outlook.com>
2016-12-18 20:54:06 +0800

    Before split app.module.ts

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+//import { Observable } from 'rxjs/Observable';
+//import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,4 +13,14 @@ export class HeroService {
 	getHeroes(): Promise<Hero[]> {
 		return Promise.resolve(HEROES);
 	}
+
+	//getHeroesAcync(): Observable<any> {
+	//	return Observable.of(HEROES);
+    //}
+
+	getHero(id: number): Promise<Hero> {
+		return this.getHeroes()
+				.then(heroes => heroes.find(hero => hero.id === id));
+	}
+
 }
